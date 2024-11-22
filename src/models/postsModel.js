863e193e167f352fad2e7b89154c19f770f120df
Aline Ometto <aline.ometto90@gmail.com>
2024-11-22 15:15:55 -0300

    Add files via upload

---
+++ src/models/postsModel.js
@@ -0,0 +1,20 @@
+import conectarAoBanco from "../config/dbConfig.js";
+
+// Conecta ao banco de dados utilizando a string de conexão do ambiente
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+// Função assíncrona para buscar todos os posts do banco de dados
+export async function getTodosPosts(){
+    // Seleciona o banco de dados "imersao-instabytes"
+    const db = conexao.db("imersao-instabytes");
+    // Seleciona a coleção "posts"
+    const colecao = db.collection("posts");
+    // Retorna um array com todos os documentos da coleção
+    return colecao.find().toArray();
+  };
+
+  export async function criarPost(novoPost){
+        const db = conexao.db("imersao-instabytes");
+        const colecao = db.collection("posts");
+        return colecao.insertOne(novoPost);
+  };
---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,39 @@
+import express from "express"; // Importa o módulo Express para criar o servidor web
+import multer from "multer"; // Importa o módulo Multer para lidar com uploads de arquivos
+
+// Importa funções controladoras do arquivo postController.js
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postController.js";
+
+// Configura o armazenamento de arquivos para o Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Define o diretório de destino para os uploads: 'uploads/'
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Define o nome do arquivo como o nome original
+    cb(null, file.originalname);
+  }
+})
+
+// Cria uma instância do Multer com o armazenamento configurado
+const upload = multer({ dest: "./uploads", storage })
+
+// Função para definir as rotas da aplicação
+const routes = (app) => {
+  // Habilita o parsing de JSON no corpo das requisições para entender dados enviados no formato JSON
+  app.use(express.json());
+
+  // Rota GET para listar todos os posts (implementação na função listarPosts)
+  app.get("/posts", listarPosts);
+
+  // Rota POST para criar um novo post (implementação na função postarNovoPost)
+  app.post("/posts", postarNovoPost);
+
+  // Rota POST para upload de imagem (usa o middleware upload.single("imagem"))
+  // e chama a função uploadImagem para processar a imagem
+  app.post("/upload", upload.single("imagem"), uploadImagem);
+};
+
+// Exporta a função routes para uso em outros arquivos do projeto
+export default routes;
