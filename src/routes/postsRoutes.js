1ac18983082448824555fad2fde2fc7c1f8073cb
Aline Ometto <aline.ometto90@gmail.com>
2024-11-24 11:40:34 -0300

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,8 +1,12 @@
 import express from "express"; // Importa o módulo Express para criar o servidor web
 import multer from "multer"; // Importa o módulo Multer para lidar com uploads de arquivos
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postController.js";// Importa funções controladoras do arquivo postController.js
+import cors from "cors";
 
-// Importa funções controladoras do arquivo postController.js
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postController.js";
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200
+}
 
 // Configura o armazenamento de arquivos para o Multer
 const storage = multer.diskStorage({
@@ -14,15 +18,16 @@ const storage = multer.diskStorage({
     // Define o nome do arquivo como o nome original
     cb(null, file.originalname);
   }
-})
+});
 
 // Cria uma instância do Multer com o armazenamento configurado
-const upload = multer({ dest: "./uploads", storage })
+const upload = multer({ storage: storage});
 
 // Função para definir as rotas da aplicação
 const routes = (app) => {
   // Habilita o parsing de JSON no corpo das requisições para entender dados enviados no formato JSON
   app.use(express.json());
+  app.use(cors(corsOptions));
 
   // Rota GET para listar todos os posts (implementação na função listarPosts)
   app.get("/posts", listarPosts);
@@ -33,6 +38,8 @@ const routes = (app) => {
   // Rota POST para upload de imagem (usa o middleware upload.single("imagem"))
   // e chama a função uploadImagem para processar a imagem
   app.post("/upload", upload.single("imagem"), uploadImagem);
+
+  app.put("/upload/:id", atualizarNovoPost);
 };
 
 // Exporta a função routes para uso em outros arquivos do projeto
